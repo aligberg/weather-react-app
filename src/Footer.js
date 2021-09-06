71524495a3123c0f9f1a730f1940716f756ab446
Ali Greenberg <abgreenb2@gmail.com>
2021-09-06 14:44:29 -0400

    Added CSS files for each component

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/aligberg/weather-app"
+        target="_blank"
+        rel="noreferrer "
+      >
+        Open-source code
+      </a>
+      by Ali Greenberg
+    </footer>
+  );
+}
