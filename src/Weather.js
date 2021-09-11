b2abba4c5aef8654098fdae4f8334a715956f6e7
Ali Greenberg <abgreenb2@gmail.com>
2021-09-11 13:03:55 -0400

    Integrated weather API and added state

--- src/Weather.js
+++ src/Weather.js
@@ -1,117 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 
 
-export default function Weather() {
- 
-  let weatherData = {
-    city: "New York City",
-    temperature: 70,
-    feelsLike: 71,
-    date: "Sunday, August 29 2021",
-    time: "11:40",
-    description: "Overcast",
-    weatherImg: "http://openweathermap.org/img/wn/04d@2x.png",
-    humidity: 81,
-    wind: 1,
-    max: 76,
-    min: 70
-  };
-  return (
-    <div className="Weather">
-      <div className="row date-time">
-        <div className="col-6">
-          <div className="date" id="date">
-            {weatherData.date}
-          </div>
-        </div>
-        <div className="col-6">
-          <div className="time" id="time">
-            {weatherData.time}
-          </div>
-        </div>
-      </div>
-      <form className="Search mt-2">
-      <div className="form-row search-bar" autofocus="off">
-        <div className="col-10 bar">
-          <input
-            className="form-control search"
-            type="search"
-            placeholder="Search cities..."
-            aria-label="Search"
-            autocomplete="off"
-          />
-        </div>
-        <div className="icons col-auto">
-          <button type="submit" value="search" className="btn fabutton">
-            <i className="fas fa-search searchIcon p-1"></i>
-          </button>
-          <button type="submit" class="btn fabutton" id="geolocation">
-            <i className="fas fa-map-marker-alt locationIcon p-1"></i>
-          </button>
-        </div>
-      </div>
-    </form>
-      <div className="row city-name">
-        <h2>{weatherData.city}</h2>
-      </div>
-      <div className="row city-highlights">
-        <div className="col-6">
-          <div className="row">
-            <div className="col-6">
-              <div className="temp">
-                <h1>
-                  <span>{weatherData.temperature}</span>
-                  <sup>
-                    <small>
-                      °<a href="/">F</a> / <a href="/">C</a>
-                    </small>
-                  </sup>
-                </h1>
-              </div>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  Feels like <span>{weatherData.feelsLike}°</span>
-                </li>
-                <li>
-                  Hi/Lo:{" "}
-                  <span>
-                    {weatherData.max}°/{weatherData.min}°
-                  </span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      feelsLike: response.data.main.feels_like,
+      date: "Sunday, August 29 2021",
+      time: "11:40",
+      description: response.data.weather[0].description,
+      weatherImg: "http://openweathermap.org/img/wn/04d@2x.png",
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      max: response.data.main.temp_max,
+      min: response.data.main.temp_min
+    });
+  }
+  
 
-        <div className="col-6 conditions">
-          <div className="row">
-            <div class="col-6">
-              <img
-                src={weatherData.weatherImg}
-                alt={weatherData.description}
-                id="icon"
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <div className="row date-time">
+          <div className="col-6">
+            <div className="date" id="date">
+              {weatherData.date}
+            </div>
+          </div>
+          <div className="col-6">
+            <div className="time" id="time">
+              {weatherData.time}
+            </div>
+          </div>
+        </div>
+        <form className="Search mt-2">
+          <div className="form-row search-bar" autofocus="off">
+            <div className="col-10 bar">
+              <input
+                className="form-control search"
+                type="search"
+                placeholder="Search cities..."
+                aria-label="Search"
+                autoComplete="off"
+                autoFocus="on"
               />
             </div>
-            <div className="col-6">
-              <ul>
-                <li>
-                  <span>{weatherData.description}</span>
-                </li>
-                <li>
-                  Humidity: <span>{weatherData.humidity}%</span>
-                </li>
-                <li>
-                  Wind: <span>{weatherData.wind} km/h</span>
-                </li>
-              </ul>
+            <div className="icons col-auto">
+              <button type="submit" value="search" className="btn fabutton">
+                <i className="fas fa-search searchIcon p-1"></i>
+              </button>
+              <button type="submit" className="btn fabutton" id="geolocation">
+                <i className="fas fa-map-marker-alt locationIcon p-1"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="row city-name">
+          <h2>{weatherData.city}</h2>
+        </div>
+        <div className="row city-highlights">
+          <div className="col-6">
+            <div className="row">
+              <div className="col-6">
+                <div className="temp">
+                  <h1>
+                    <span>{Math.round(weatherData.temperature)}</span>
+                    <sup>
+                      <small>
+                        °<a href="/">F</a> / <a href="/">C</a>
+                      </small>
+                    </sup>
+                  </h1>
+                </div>
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    Feels like <span>{Math.round(weatherData.feelsLike)}°</span>
+                  </li>
+                  <li>
+                    Hi/Lo:{" "}
+                    <span>
+                      {Math.round(weatherData.max)}°/{Math.round(weatherData.min)}°
+                  </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-6 conditions">
+            <div className="row">
+              <div className="col-6">
+                <img
+                  src={weatherData.weatherImg}
+                  alt={weatherData.description}
+                  id="icon"
+                />
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>
+                    <span className="text-capitalize">{weatherData.description}</span>
+                  </li>
+                  <li>
+                    Humidity: <span>{weatherData.humidity}%</span>
+                  </li>
+                  <li>
+                    Wind: <span>{Math.round(weatherData.wind)} km/h</span>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "54cae2bb0d0b7168b158d795db1580ea";
+    let unit = "imperial";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
